d57d509be9b931a9040065de6b379ed1634a28a7
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2024-05-26 19:09:27 +0100

    added getting the jobs, displaying on cards with search functionality

--- app/(dashboard)/jobs/page.tsx
+++ app/(dashboard)/jobs/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import JobsList from "@/components/JobsList";
+import SearchForm from "@/components/SearchForm";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { getAllJobsAction } from "@/utils/actions";
 
-const JobsPage = () => {
-  return <h1 className="text-4xl">JobsPage</h1>;
-};
+async function AllJobsPage() {
+  const queryClient = new QueryClient();
 
-export default JobsPage;
+  await queryClient.prefetchQuery({
+    queryKey: ["jobs", "", "all", 1],
+    queryFn: () => getAllJobsAction({}),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <SearchForm />
+      <JobsList />
+    </HydrationBoundary>
+  );
+}
+
+export default AllJobsPage;
