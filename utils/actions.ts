9d3cfc1f52bfb58216dacf86dacd0ca7d703047f
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2024-05-27 09:21:45 +0100

    add pagination

--- utils/actions.ts
+++ utils/actions.ts
@@ -37,7 +37,7 @@ export async function getAllJobsAction({
   search,
   jobStatus,
   page = 1,
-  limit = 10,
+  limit = 4,
 }: GetAllJobsActionTypes): Promise<{
   jobs: JobType[];
   count: number;
@@ -73,15 +73,21 @@ export async function getAllJobsAction({
         status: jobStatus,
       };
     }
+    const skip = (page - 1) * limit;
 
     const jobs: JobType[] = await prisma.job.findMany({
       where: whereClause,
+      skip,
+      take: limit,
       orderBy: {
         createdAt: "desc",
       },
     });
-
-    return { jobs, count: 0, page: 1, totalPages: 0 };
+    const count: number = await prisma.job.count({
+      where: whereClause,
+    });
+    const totalPages = Math.ceil(count / limit);
+    return { jobs, count, page, totalPages };
   } catch (error) {
     console.error(error);
     return { jobs: [], count: 0, page: 1, totalPages: 0 };
